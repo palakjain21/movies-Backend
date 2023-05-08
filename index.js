bf736a24ad6635901acc76bd40c2b19871234bf4
palakjain21 <21palak2001@gmail.com>
2023-05-08 17:51:01 +0530

    cleaned the code

--- index.js
+++ index.js
@@ -1,34 +1,3 @@
-// const express = require("express");
-// const cors = require("cors");
-// const { Client } = require("@notionhq/client");
-
-// const notion = new Client({
-//   auth: "secret_BJAWekAPIOMfXPd1TzImGLBjjaCdsaxqWrcaryEEeAP",
-// });
-// const databaseRouter = require("./db_router");
-// const app = express();
-// const PORT = 8000;
-// const HOST = "localhost";
-
-// (async () => {
-//   const database_id = "737ac0f9817047ad81f11c543185e547";
-
-//   const response = await notion.databases.retrieve({ database_id });
-//   console.log(response);
-// })();
-// // const getDatabaseSchema = async (req, res) => {
-// //   const database_id = "a189704256dc453db3eb37ebb1f3caf8";
-// //   const response = await notion.databases.retrieve({
-// //     database_id,
-// //   });
-// //   console.log(response);
-// //   res.json(response);
-// // };
-
-// // app.use("/", databaseRouter);
-// app.listen(() => {
-//   console.log("server connected");
-// });
 const express = require("express");
 const cors = require("cors");
 const { Client } = require("@notionhq/client");
@@ -52,6 +21,5 @@ app.use(
 app.use("/", databaseRouter);
 
 app.listen(PORT, () => {
-  // console.log("server started at ", env.NOTION_TOKEN);
   console.log("server started at ", PORT);
 });
